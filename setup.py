fd9d714419104a85cac99ebed0cddd1ff40fffec
Ibrahim Animashaun <iaanimashaun@gmail.com>
2023-10-12 11:22:04 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mediapipe',
+    version='0.0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    author='Daniel Fiuza & Ibrahim Animashaun',
+    
+)
